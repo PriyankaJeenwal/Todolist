91d63834a4dc99978a94dc26fbfd128bcff2b802
PriyankaJeenwal <167520462+PriyankaJeenwal@users.noreply.github.com>
2024-05-31 16:06:46 +0530

    Add files via upload

---
+++ cloudapigateway/src/test/java/com/cloudgateway/cloudapigateway/CloudapigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudgateway.cloudapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudapigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
